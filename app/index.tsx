d041e18352b69fbabd3fcaacfa98276934370390
Bhawesh Verma <bhaweshverma50@gmail.com>
2024-06-02 17:40:55 +0530

    fix: changed main page title

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ import { Link } from 'expo-router';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white text-3xl">Torrento</Text>
+      <Text className="text-white text-3xl">Torrento App</Text>
       <StatusBar style="auto" />
       <Link href="/home" className="rounded-md px-4 py-2 mt-4 text-md font-bold bg-white">
         Login
